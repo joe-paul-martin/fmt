e6647541c39b03f1ce7835eba6845ad0e9838682
joe-paul-martin <joepaul.martin1448@gmail.com>
2025-05-05 15:15:47 +0530

    still working on print.go

--- print.go
+++ print.go
@@ -5,7 +5,9 @@
 package fmt
 
 import (
+	"reflect"
 	"strconv"
+	"sync"
 	"unicode/utf8"
 )
 
@@ -102,4 +104,45 @@ func (b *buffer) writeByte(c byte) {
 
 func (b *buffer) writeRune(r rune) {
 	*b = utf8.AppendRune(*b, r)
+}
+
+// pp is used to store a printer's state and is reused with sync.Pool to avoid allocations.
+type pp struct {
+	buf buffer
+
+	// arg holds the current item, as an interface{}.
+	arg any
+
+	// value is used instead of arg for reflect values.
+	value reflect.Value
+
+	// fmt is used to format basic items such as integers or strings.
+	fmt fmt
+
+	// reordered records whether the format string used argument reordering.
+	reordered bool
+	// goodArgNum records whether the most recent reordering directive was valid.
+	goodArgNum bool
+	// panicking is set by catchPanic to avoid infinite panic, recover, panic, ... recursion.
+	panicking bool
+	// erroring is set when printing an error string to guard against calling handleMethods.
+	erroring bool
+	// wrapErrs is set when the format string may contain a %w verb.
+	wrapErrs bool
+	// wrappedErrs records the targets of the %w verb.
+	wrappedErrs []int
+}
+
+var ppFree = sync.Pool{
+	New: func() any { return new(pp) },
+}
+
+// newPrinter allocates a new pp struct or grabs a cached one.
+func newPrinter() *pp {
+	p := ppFree.Get().(*pp)
+	p.panicking = false
+	p.erroring = false
+	p.wrapErrs = false
+	p.fmt.init(&p.buf)
+	return p
 }
